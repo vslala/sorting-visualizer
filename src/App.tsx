818d4d52a1fb6b9a7c6b09e52323ddc25aece268
Varun shrivastava <varunshrivastava007@gmail.com>
2020-10-17 12:43:57 +0530

    add quick sort and optimized code for async flows

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,8 @@ function App() {
     const [arraySize, setArraySize] = useState<number>(10);
     const [speed, setSpeed] = useState<number>(600);
     const [isRunning, setIsRunning] = useState<boolean>(false);
-    let delay: number = 0;
+    const [timeElapsed, setTimeElapsed] = useState<number>(0);
+    const [info, setInfo] = useState<any>(<></>);
 
     const refreshBars = () => {
         let arr = [];
@@ -39,146 +40,244 @@ function App() {
     const refresh = () => {
         setIsRunning(true);
         console.log("refreshing...")
-        delay = 0;
+        setTimeElapsed(0);
         dispatch({type: "REFRESH", payload: refreshBars()});
         setIsRunning(false);
     }
 
     const selectionSort = async () => {
         setIsRunning(true);
-        console.log("Selection Sort the array...");
+        let startTime = new Date().getTime();
+        setInfo(<div>
+            <h2>Average Time Complexity: <strong>O(n²)</strong></h2><p>Bubble sort, sometimes referred to as sinking sort,
+            is a simple sorting algorithm that repeatedly steps through the list,
+            compares adjacent elements and swaps them if they are in the wrong order.
+            The pass through the list is repeated until the list is sorted.</p><p>In computer science, selection sort is an in-place comparison sorting algorithm.
+            It has an O(n²) time complexity, which makes it inefficient on large lists,
+            and generally performs worse than the similar insertion sort</p>
+        </div>);
+
+
         let barsCopy = [...bars];
         for (let i = 0; i < barsCopy.length - 1; i++) {
-            setTimeout(() => {
-                barsCopy[i].color = STEEL_BLUE;
-                dispatch({type: SORT, payload: [...barsCopy]});
-            }, delay += speed);
+            barsCopy[i].color = STEEL_BLUE;
+            dispatch({type: SORT, payload: [...barsCopy]});
+            await wait(speed);
 
             for (let j = i + 1; j < barsCopy.length; j++) {
-                setTimeout(() => {
-                    barsCopy[j].color = YELLOW;
-                    if (barsCopy[i].y > barsCopy[j].y) {
-                        barsCopy[i].color = GREEN;
-                        barsCopy[j].color = GREEN;
+                barsCopy[j].color = YELLOW;
+                if (barsCopy[i].y > barsCopy[j].y) {
+                    barsCopy[i].color = GREEN;
+                    barsCopy[j].color = GREEN;
 
-                        swap(barsCopy, i, j);
-                        dispatch({type: SORT, payload: [...barsCopy]});
-                    }
-
+                    swap(barsCopy, i, j);
                     dispatch({type: SORT, payload: [...barsCopy]});
-                    barsCopy[j].color = BLACK;
-                }, delay += speed);
+                }
 
+                dispatch({type: SORT, payload: [...barsCopy]});
+                barsCopy[j].color = BLACK;
+
+                await wait(speed);
             }
 
-            setTimeout(() => {
-                barsCopy[i].color = BLACK;
-                dispatch({type: SORT, payload: [...barsCopy]});
-            }, delay += speed);
+            barsCopy[i].color = BLACK;
+            dispatch({type: SORT, payload: [...barsCopy]});
+            await wait(speed);
         }
 
-        paintSteelBlue(barsCopy);
+        await paintSteelBlue(barsCopy);
 
-        setTimeout(() => {
-            setIsRunning(false);
-        }, delay += speed);
-
-        dispatch({type: SORT, payload: [...barsCopy]});
+        setIsRunning(false);
+        setTimeElapsed(new Date().getTime() - startTime);
+        await wait(speed);
     }
 
-    const bubbleSort = () => {
+    const bubbleSort = async () => {
         setIsRunning(true);
-        console.log("Bubble Sort the array...");
+        let startTime = new Date().getTime();
+        setInfo(<div>
+            <h2>Average Time Complexity: <strong>O(n²)</strong></h2>
+            <p>Bubble sort, sometimes referred to as sinking sort,
+            is a simple sorting algorithm that repeatedly steps through the list,
+            compares adjacent elements and swaps them if they are in the wrong order.
+            The pass through the list is repeated until the list is sorted.</p>
+        </div>)
+
         let arr = [...bars];
         for (let i = 0; i < arr.length; i++) {
             for (let j = 0; j < arr.length - i - 1; j++) {
 
-                setTimeout(() => {
-                    arr[j].color = STEEL_BLUE;
-                    arr[j + 1].color = YELLOW;
-                    dispatch({type: SORT, payload: [...arr]});
-                }, delay += speed);
+                arr[j].color = STEEL_BLUE;
+                arr[j + 1].color = YELLOW;
+                dispatch({type: SORT, payload: [...arr]});
+                await wait(speed);
 
-                setTimeout(() => {
-                    if (arr[j].y > arr[j + 1].y) {
-                        arr[j].color = GREEN;
-                        arr[j + 1].color = GREEN;
-                        swap(arr, j, j + 1);
-                        dispatch({type: SORT, payload: [...arr]});
-                    }
-                }, delay += speed);
-
-                setTimeout(() => {
-                    arr[j].color = BLACK;
-                    arr[j + 1].color = BLACK;
+                if (arr[j].y > arr[j + 1].y) {
+                    arr[j].color = GREEN;
+                    arr[j + 1].color = GREEN;
+                    swap(arr, j, j + 1);
                     dispatch({type: SORT, payload: [...arr]});
-                }, delay += speed);
+                }
+                await wait(speed);
+
+                arr[j].color = BLACK;
+                arr[j + 1].color = BLACK;
+                dispatch({type: SORT, payload: [...arr]});
+                await wait(speed);
 
             }
         }
 
-        paintSteelBlue(arr);
+        await paintSteelBlue(arr);
 
-        setTimeout(() => {
-            setIsRunning(false);
-        }, delay += speed);
-
-        console.log(arr);
-        // dispatch({type: SORT, payload: [...arr]});
+        setIsRunning(false);
+        setTimeElapsed(new Date().getTime() - startTime);
+        await wait(speed);
     }
 
-    function paintSteelBlue(arr: any[]) {
+    async function paintSteelBlue(arr: any[]) {
         for (let i = 0; i < arr.length; i++) {
-            setTimeout(() => {
-                arr[i].color = STEEL_BLUE;
-                dispatch({type: SORT, payload: [...arr]});
-            }, delay += speed);
+            arr[i].color = STEEL_BLUE;
+            dispatch({type: SORT, payload: [...arr]});
+            await wait(speed);
         }
     }
 
-    const insertionSort = () => {
+    const insertionSort = async () => {
         setIsRunning(true);
+        let startTime = new Date().getTime();
+        setInfo(<div>
+            <h2>Average Time Complexity: <strong>O(n²)</strong></h2>
+            <p>Insertion sort is a simple sorting algorithm that builds the final sorted array one item at a time.
+                It is much less efficient on large lists than more advanced algorithms such as quicksort,
+                heapsort, or merge sort. </p>
+        </div>)
+
         let arr = [...bars];
         for (let i = 0; i < arr.length; i++) {
-            setTimeout(() => {
-                arr[i].color = STEEL_BLUE;
-                dispatch({type: SORT, payload: [...arr]})
-            }, delay += speed);
+            arr[i].color = STEEL_BLUE;
+            dispatch({type: SORT, payload: [...arr]})
+            await wait(speed);
 
             for (let j = i; j > 0; j--) {
-                setTimeout(() => {
-                    arr[j - 1].color = YELLOW;
-                    dispatch({type: SORT, payload: [...arr]})
-                }, delay += speed);
+                arr[j - 1].color = YELLOW;
+                dispatch({type: SORT, payload: [...arr]})
+                await wait(speed);
 
-                setTimeout(() => {
-                    if (arr[j].y < arr[j - 1].y) {
-                        arr[j].color = GREEN;
-                        arr[j - 1].color = GREEN;
-                        swap(arr, j, j - 1);
-                        dispatch({type: SORT, payload: [...arr]});
-                    }
-                }, delay += speed);
+                if (less(arr, j, j - 1)) {
+                    arr[j].color = GREEN;
+                    arr[j - 1].color = GREEN;
+                    swap(arr, j, j - 1);
+                    dispatch({type: SORT, payload: [...arr]});
+                } else break;
+                await wait(speed);
 
-
-                setTimeout(() => {
-                    arr[j - 1].color = BLACK;
-                    dispatch({type: SORT, payload: [...arr]})
-                }, delay += speed);
-
+                arr[j - 1].color = BLACK;
+                dispatch({type: SORT, payload: [...arr]})
+                await wait(speed);
             }
 
-            setTimeout(() => {
-                arr[i].color = BLACK;
-                dispatch({type: SORT, payload: [...arr]})
-            }, delay += speed);
+            arr[i].color = BLACK;
+            dispatch({type: SORT, payload: [...arr]})
+            await wait(speed);
         }
 
-        paintSteelBlue(arr);
+        await paintSteelBlue(arr);
 
-        setTimeout(() => {
-            setIsRunning(false);
-        }, delay += speed);
+        await wait(speed);
+        setIsRunning(false);
+        setTimeElapsed(new Date().getTime() - startTime);
+    }
+
+    const quickSortOrchestrator = async () => {
+        setIsRunning(true);
+        let startTime = new Date().getTime();
+        setInfo(<div>
+            <h2>Average Time Complexity: <strong>O(n log n)</strong></h2>
+            <p>Quicksort is an efficient sorting algorithm.
+                Developed by British computer scientist Tony Hoare in 1959 and published in 1961,
+                it is still a commonly used algorithm for sorting. When implemented well,
+                it can be about two or three times faster than its main competitors,
+                merge sort and heapsort.
+            </p>
+        </div>)
+
+        let arr = [...bars];
+        await quickSort(arr, 0, arr.length, 0, 0, 0, 0);
+        await paintSteelBlue(arr);
+
+        setIsRunning(false);
+        setTimeElapsed(new Date().getTime() - startTime);
+    }
+
+    const quickSort = async (arr: any, start: number, end: number, pivot: number, randomPivot: number, i: number, j: number) => {
+        if (start === end) {
+            return;
+        }
+
+        arr[start].color = STEEL_BLUE;
+        arr[end - 1].color = STEEL_BLUE;
+        dispatch({type: SORT, payload: [...arr]});
+        await wait(speed);
+
+
+        randomPivot = start + randomNumber(start, end);
+        arr[randomPivot].color = YELLOW;
+        dispatch({type: SORT, payload: [...arr]});
+        await wait(speed);
+
+
+        pivot = end - 1;
+        swap(arr, randomPivot, pivot);
+        arr[randomPivot].color = BLACK;
+        arr[pivot].color = YELLOW;
+        dispatch({type: SORT, payload: [...arr]});
+
+        await wait(speed);
+        i = start;
+        j = i;
+        while (j < pivot) {
+            if (less(arr, j, pivot)) {
+                arr[j].color = GREEN;
+                arr[pivot].color = GREEN;
+                dispatch({type: SORT, payload: [...arr]});
+
+                await wait(speed);
+                swap(arr, i, j);
+                i++;
+            }
+            j++;
+        }
+
+        swap(arr, i, pivot);
+        arr[i].color = GREEN;
+        dispatch({type: SORT, payload: [...arr]});
+        await wait(speed);
+
+
+        // put the pivot in its correct position
+        pivot = i;
+
+        await quickSort(arr, start, pivot, 0, 0, 0, 0);
+        await quickSort(arr, pivot + 1, end, 0, 0, 0, 0);
+
+        arr[j].color = BLACK;
+        arr[pivot].color = BLACK;
+        arr[start].color = BLACK;
+        arr[end - 1].color = BLACK;
+        dispatch({type: SORT, payload: [...arr]});
+        await wait(speed);
+
+    }
+
+    const wait = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+    const randomNumber = (min: number, max: number) => {
+        return (Math.floor(Math.random() * (max - min)));
+    }
+
+    const less = (arr: any, index1: number, index2: number) => {
+        return arr[index1].y < arr[index2].y;
     }
 
     const swap = (arr: any, index1: number, index2: number) => {
@@ -187,11 +286,11 @@ function App() {
         arr[index2].y = temp;
     }
 
+
     return (
         <>
             <div className="container">
                 <div className="row">
-                    <div className="col"></div>
                     <div className="col">
 
                         <div className="App">
@@ -205,11 +304,19 @@ function App() {
                         </div>
 
                     </div>
-                    <div className="col"></div>
+                </div>
+                <div className="row">
+                    <div className="col">
+                        {
+                            timeElapsed > 0 ?
+                                <h1>Total Time Taken = {Math.round(timeElapsed / 1000)} seconds</h1> : <></>
+                        }
+                        {info}
+                    </div>
                 </div>
             </div>
             <nav className="navbar fixed-bottom navbar-light bg-light ">
-                <a className="navbar-brand" href="#">Sorting Visualizer</a>
+                <div className="navbar-brand">Sorting Visualizer</div>
                 <div className="btn-toolbar mb-3" role="toolbar" aria-label="Toolbar with button groups">
                     <label htmlFor="btn-group">Size</label><br/>
                     <div className="btn-group btn-group-sm">
@@ -251,6 +358,9 @@ function App() {
                 </button>
                 <button type={"button"} disabled={isRunning} className={"btn btn-outline-dark"}
                         onClick={insertionSort}>Insertion Sort
+                </button>
+                <button type={"button"} disabled={isRunning} className={"btn btn-outline-dark"}
+                        onClick={quickSortOrchestrator}>Quick Sort
                 </button>
             </nav>
         </>
